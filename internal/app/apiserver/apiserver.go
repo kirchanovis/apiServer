e6f4bbf98a613d547e9e1c1ffe1520f2f28a5925
ikirchanov <ikirchanov@7tech.ru>
2020-06-24 23:19:32 +0300

    second

--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -1,10 +1,13 @@
 package apiserver
 
 type APIServer struct {
+	config *Config
 }
 
-func New() *APIServer {
-	return &APIServer{}
+func New(config *Config) *APIServer {
+	return &APIServer{
+		config: config,
+	}
 }
 
 func (s *APIServer) Start() error {
